964842b0cecd8471d84e2bc3eb64183681b4d380
Christoph Rieke <christoph.rieke@up42.com>
2020-03-05 16:45:15 +0100

    fix e2e helper import

--- e2e.py
+++ e2e.py
@@ -10,7 +10,7 @@ import sys
 import geojson
 
 sys.path.insert(
-    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "blocks/sharpening/src"))
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "src"))
 )
 from helpers import setup_test_directories  # pylint: disable=wrong-import-position
 
